b4004475db3285d0eca5f46490f9e42705320cf7
“crlnvls” <cveloso@hotmail.co.uk>
2023-12-05 23:17:56 +0000

    Add src structure

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <></>
+  );
+};
+
+export default NotFound;
