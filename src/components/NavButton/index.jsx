24208b273a0806fc7a19a9626a8672feaec212c4
“crlnvls” <cveloso0808@gmail.com>
2023-12-06 18:30:21 +0000

    Create Nav Button files

---
+++ src/components/NavButton/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./NavButton.module.css";
+
+
+const NavButton = () => {
+  return (
+    <>
+    <button>Click me</button>
+    </>
+  );
+};
+
+export default NavButton;
