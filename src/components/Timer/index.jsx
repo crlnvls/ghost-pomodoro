1e1e5a5bb1c3eb2cb1b56571565dbccc216acd3d
“crlnvls” <cveloso0808@gmail.com>
2023-12-07 17:13:34 +0000

    Add timer component and styles

---
+++ src/components/Timer/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Timer.css"
+
+const Timer = () => {
+  return (
+    <>
+        <div class="timer-display">
+            <div class="type">Pomodoro</div>
+            <div class="time">25:00</div>
+        </div>
+ 
+    </>
+  );
+};
+
+export default Timer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,20 +3,19 @@ import React from "react";
 import "./Home.css";
 import NavButton from "../../components/NavButton";
 import Ghost from "../../components/Ghost";
+import Timer from "../../components/Timer";
 
 const Home = () => {
   return (
     <>
-
-<div  className="center-btn"> 
-
-    <NavButton>History</NavButton>
-
-    <NavButton>Settings</NavButton>
-     
-</div>
-    <h1>Ghost Pomodoro 👻</h1>
-<Ghost></Ghost>
+      <div  className="center-btn"> 
+        <NavButton>History</NavButton>
+        <NavButton>Settings</NavButton>
+      </div>
+      <div className="center-display">
+        <Ghost></Ghost>
+        <Timer></Timer>
+      </div>
     </>
   );
 };
