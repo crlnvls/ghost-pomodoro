04bf7ffe372b20802c23de0c66eeb4e31c093cc4
“crlnvls” <cveloso0808@gmail.com>
2023-12-07 19:25:30 +0000

    Add styling time and type

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -6,8 +6,12 @@ const Timer = () => {
   return (
     <>
         <div className="timer-display">
-            <div className="type">Pomodoro</div>
-            <div className="time">25:00</div>
+            <div className="type">
+              Pomodoro
+            </div>
+            <div className="time">
+             25:00
+            </div>
         </div>
  
     </>
