fafa6ab1cd415818912d705dcfd0c7d5354c838c
“crlnvls” <cveloso0808@gmail.com>
2023-12-07 20:45:18 +0000

    Add svg next btn

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
+
 import "./Timer.css"
+import NextSvg from "../NextSvg";
+
 
 const Timer = () => {
   return (
     <>
-        <div className="timer-display">
+      <div className="timer-display">
+        <div className="type-display">
             <div className="type">
               Pomodoro
             </div>
+          <button className="next-btn">
+             <NextSvg/>
+            </button>
+        </div>
             <div className="time">
              25:00
             </div>
-        </div>
+      </div>
+     
  
     </>
   );
