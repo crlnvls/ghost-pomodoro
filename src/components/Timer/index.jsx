b1de434d0333f96d7500bf00af6d91e59e107853
“crlnvls” <cveloso0808@gmail.com>
2023-12-24 09:14:38 +0000

    Add pause and resume functionalities

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -19,6 +19,8 @@ const Timer = () => {
   const [countdown, setCountdown] = useState(null);
   const [stepIndex, setStepIndex] = useState(0);
   const [isStarted, setIsStarted] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(0);
+  const [endTime, setEndTime] = useState(null);
 
   const pomodoroFlow = [
     options[0], // pomodoro
@@ -48,11 +50,13 @@ const Timer = () => {
 
   const startTimer = () => {
     const currentOption = pomodoroFlow[stepIndex];
-    const endTime = new Date(
+    const calculatedEndTime = new Date(
       Date.parse(new Date()) + getMillisecondsFromTime(currentOption.time)
     );
 
-    let { total, minutes, seconds } = getTimeRemaining(endTime);
+    setEndTime(calculatedEndTime);
+
+    let { total, minutes, seconds } = getTimeRemaining(calculatedEndTime);
 
     setTimer(
       `${minutes.toString().padStart(2, "0")}:${seconds
@@ -61,7 +65,71 @@ const Timer = () => {
     );
 
     const countdown = setInterval(() => {
-      ({ total, minutes, seconds } = getTimeRemaining(endTime));
+      ({ total, minutes, seconds } = getTimeRemaining(calculatedEndTime));
+
+      if (total <= 0) {
+        clearInterval(countdown);
+
+        setTimer("00:00");
+
+        setTimeout(() => {
+          moveToNextStep();
+        }, 1000);
+      } else {
+        setTimer(
+          `${minutes.toString().padStart(2, "0")}:${seconds
+            .toString()
+            .padStart(2, "0")}`
+        );
+      }
+    }, 1000);
+
+    setCountdown(countdown);
+    setIsStarted(true);
+  };
+
+  const getMillisecondsFromTime = (time) => {
+    const [minutes, seconds] = time.split(":").map(Number);
+    return (minutes * 60 + seconds) * 1000;
+  };
+
+  const moveToNextStep = () => {
+    const nextIndex = stepIndex + 1;
+    if (nextIndex < pomodoroFlow.length) {
+      setStepIndex(nextIndex);
+      setActive(pomodoroFlow[nextIndex].label);
+    } else {
+      console.log("Pomodoro flow completed");
+      // Optionally reset the flow or perform any other action upon completion
+    }
+  };
+
+  const pauseTimer = () => {
+    clearInterval(countdown);
+    const currentTime = new Date();
+    setRemainingTime(Date.parse(endTime) - Date.parse(currentTime));
+    setIsStarted(false);
+  };
+
+  const resumeTimer = () => {
+    const now = new Date();
+    const newEnd = new Date(now.getTime() + remainingTime);
+
+    setEndTime(newEnd);
+
+    let { total, minutes, seconds } = getTimeRemaining(newEnd);
+
+    setTimer(
+      `${minutes.toString().padStart(2, "0")}:${seconds
+        .toString()
+        .padStart(2, "0")}`
+    );
+
+    setRemainingTime(null);
+    setIsStarted(true);
+
+    const countdown = setInterval(() => {
+      ({ total, minutes, seconds } = getTimeRemaining(newEnd));
 
       if (total <= 0) {
         clearInterval(countdown);
@@ -83,26 +151,6 @@ const Timer = () => {
     setCountdown(countdown);
   };
 
-  const getMillisecondsFromTime = (time) => {
-    const [minutes, seconds] = time.split(":").map(Number);
-    return (minutes * 60 + seconds) * 1000;
-  };
-
-  const moveToNextStep = () => {
-    const nextIndex = stepIndex + 1;
-    if (nextIndex < pomodoroFlow.length) {
-      setStepIndex(nextIndex);
-      setActive(pomodoroFlow[nextIndex].label);
-    } else {
-      console.log("Pomodoro flow completed");
-      // Optionally reset the flow or perform any other action upon completion
-    }
-  };
-
-  const pauserTimer = () => {};
-
-  const resumeTimer = () => {};
-
   const handleSelect = (option) => {
     setSelectedOption(option);
     setTimer(option.time);
@@ -112,7 +160,15 @@ const Timer = () => {
   };
 
   const onClickStart = () => {
-    startTimer();
+    if (!isStarted) {
+      if (remainingTime) {
+        resumeTimer();
+      } else {
+        startTimer();
+      }
+    } else {
+      pauseTimer();
+    }
   };
 
   const onClickReset = () => {
@@ -120,6 +176,9 @@ const Timer = () => {
     const currentOption = pomodoroFlow[stepIndex];
     setTimer(currentOption.time);
     setCountdown(null);
+    setIsStarted(false);
+    setRemainingTime(0);
+    setEndTime(null);
   };
 
   return (
@@ -139,15 +198,9 @@ const Timer = () => {
         <div className="time">{timer}</div>
 
         <div className="center-timer-btn">
-          {isStarted ? (
-            <Button className="timer-btn" onClick={onClickStart}>
-              start
-            </Button>
-          ) : (
-            <Button className="timer-btn" onClick={onClickStart}>
-              pause
-            </Button>
-          )}
+          <Button className="timer-btn" onClick={onClickStart}>
+            {isStarted ? "pause" : remainingTime === 0 ? "start" : "resume"}
+          </Button>
 
           <Button className="timer-btn" onClick={onClickReset}>
             reset
