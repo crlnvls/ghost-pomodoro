614898bd76bb0db4b844346916812ebf2f828962
“crlnvls” <cveloso0808@gmail.com>
2023-12-26 23:02:47 +0000

    Add header

--- src/components/SettingsPopup/index.jsx
+++ src/components/SettingsPopup/index.jsx
@@ -5,14 +5,13 @@ import Modal from "../Modal";
 import Button from "../../components/Button";
 
 const SettingsPopup = () => {
-  const headerContent = "Settings";
   const bodyContent = "";
   return (
     <>
       <div className="border-left">
         <Modal
           trigger={<Button className="nav-btn">settings</Button>}
-          header={headerContent}
+          header={<h1>Settings</h1>}
           content={bodyContent}
         />
       </div>
